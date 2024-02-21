79021fd5b46ad2e34f52aa6f2ceb0c505b8ebbf5
Anish Subedi <codeth690@gmail.com>
2024-02-21 10:42:42 +0545

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const { connectDB } = require("../configurations/connectpg");
 const app=express()
 const cookieParser=require('cookie-parser')
 const {error_middleware}=require('./middleware/error.js');
@@ -6,6 +7,7 @@ const adminroute=require('./router/adminrouter.js')
 const vendorrouter=require('./router/vendorrouter.js')
 const customerrouter=require('./router/customerrouter.js')
 const connectfirebase = require('./configurations/connectfirebase.js');
+
 
 
 //for parsing
@@ -18,10 +20,34 @@ require('dotenv').config()
 app.use('/admin',adminroute)
 app.use('/vendor',vendorrouter)
 app.use('/customer',customerrouter)
-app.use('/',async (req,res)=>{
+
+
+app.use('/delete', async (req, res) => {
+    const pool=await connectDB();
+    try {
+      // Get a list of all tables
+      const result = await pool.query(`
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+          AND table_type = 'BASE TABLE'
+      `);
+  
+      // Drop each table
+      for (let row of result.rows) {
+        await pool.query(`DROP TABLE ${row.table_name} CASCADE`);
+      }
+  
+      res.status(200).send('All tables deleted');
+    } catch (error) {
+      console.error('Error deleting tables:', error);
+      res.status(500).send('Error deleting tables');
+    }
+  });
+
+  app.use('/',async (req,res)=>{
     return res.status(404).json('not found')
 })
-
 //for the errors
 app.use(error_middleware)
 
