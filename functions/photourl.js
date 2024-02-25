e876215e55217d10291af71ce7fc25eaad71f6b1
Anish Subedi <codeth690@gmail.com>
2024-02-25 21:00:24 +0545

    photo

--- functions/photourl.js
+++ functions/photourl.js
@@ -13,17 +13,18 @@ const image_url=noTryCatch(async(req,res,next)=>{
   return next(new customError("provide file",400))
   const n = req.files.length;
   const promises_url = [];
-  const {profile_menu}=req.body;
+//   const {profile_menu}=req.body;
+const profile_menu="profile"
   if(!['menu','profile'].includes(profile_menu))
   return next(new customError('menu or profile at which photo u want to add',400))
 
   let url=''
   console.log(role)
-  if(role=='customer'){
+  if(role=='customer'||role=='admin'){
     url=role
   }
   else if(role=='vendor'){
-     url=profile_menu.includes('profile')?role:role+'/'+username+'/menu';
+     url=profile_menu.includes('profile')?role+'/'+username+'/profile':role+'/'+username+'/menu';
   }
   
   for (let i = 0; i < n; i++) {
