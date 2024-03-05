f8bb585e9d48e922d3e02994fa13c0d8ad2cdcee
Anish Subedi <codeth690@gmail.com>
2024-03-05 09:00:02 +0545

    last

--- customer/menucrud.js
+++ customer/menucrud.js
@@ -17,6 +17,16 @@ query,values
   res.json(pgres.rows);
 });
 
+const display_menu_db = noTryCatch(async (req, res) => {
+  const where_conditions = ["food_name","category","price","id","r_username","discount_percentage","count"];
+  const order_conditions = ["food_name","category","price","id","r_username","discount_percentage","count"];
+  const {query,values}=await display_query('menu',where_conditions,order_conditions,req.query)
+  const client = await connectDB();
+  const pgres = await client.query(
+query,values
+  );
+  res.json(pgres.rows);
+});
 
 
-module.exports = { display_menu};
+module.exports = { display_menu,display_menu_db};
